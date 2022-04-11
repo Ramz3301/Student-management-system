e565ede8261fc73f06bb3f5acd454a1198656b37
Ramzan Kurbanov <ramkurbanov10@yandex.ru>
2022-04-11 20:54:13 +0300

    add students template

--- src/main/java/com/example/studentmanagementsystem/service/StudentServiceImpl.java
+++ src/main/java/com/example/studentmanagementsystem/service/StudentServiceImpl.java
@@ -1,5 +1,22 @@
 package com.example.studentmanagementsystem.service;
 
+import com.example.studentmanagementsystem.entity.Student;
+import com.example.studentmanagementsystem.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class StudentServiceImpl implements StudentService {
 
+    private StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
 }
