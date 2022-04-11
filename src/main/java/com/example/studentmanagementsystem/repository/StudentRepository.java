3a91bfd30ab8673776407a0df9aafe22551e7847
Ramzan Kurbanov <ramkurbanov10@yandex.ru>
2022-04-11 20:04:29 +0300

    Initial commit

---
+++ src/main/java/com/example/studentmanagementsystem/repository/StudentRepository.java
@@ -0,0 +1,12 @@
+package com.example.studentmanagementsystem.repository;
+
+import com.example.studentmanagementsystem.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+
+
+}
---
+++ src/main/java/com/example/studentmanagementsystem/service/StudentServiceImpl.java
@@ -0,0 +1,5 @@
+package com.example.studentmanagementsystem.service;
+
+public class StudentServiceImpl implements StudentService {
+
+}
